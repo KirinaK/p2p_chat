742194de24f1d3892af72fc6358a12f497719414
Kirina <pandaki.piano@gmail.com>
2018-10-30 15:19:16 +0300

    UI for chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as Peer from 'peerjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'p2pchat';
+  public textMessage: string;
+  destId: any;
+  peer = new Peer('pick-an-id');
+  conn = this.peer.connect(this.destId);
+  @ViewChild('p') p:ElementRef;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.conn.send('hello');
+  }
+
+  sendText() {
+    let pText = document.createElement('p');
+    this.p.nativeElement.innerHTML = this.textMessage;
+  }
+
+  connectServer() {
+    this.conn.on('open', function(){
+      this.conn.send('hi!');
+    });
+  }
+
+  receive() {
+    this.peer.on('connection', function(conn) {
+      conn.on('data', function(data){
+        console.log(data);
+      });
+    });
+  }
+
 }
